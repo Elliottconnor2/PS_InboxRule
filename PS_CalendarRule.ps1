8e2f423aea69a3f09f56d16851ed0702fcd9af9f
Elliottconnor2 <43493967+Elliottconnor2@users.noreply.github.com>
2022-07-25 13:11:55 +0100

    Add files via upload

---
+++ PS_CalendarRule.ps1
@@ -0,0 +1,20 @@
+# Initial run to install module, if installed continues else will install 
+If (Get-InstalledModule ExchangeOnlineManagement) {
+    write-host "Installed"
+} else {
+    Install-Module ExchangeOnlineManagement -Scope CurrentUser
+}
+# Import module to run
+Import-Module ExchangeOnlineManagement
+
+# Create sign in instance
+Connect-ExchangeOnline
+
+# Get inbox rule
+$Mailboxuser = Read-Host "Please enter the mailbox you wish to check"
+Get-InboxRule -Mailbox $Mailboxuser 
+
+# Get details of specific rule
+$MailBoxrule = Read-Host "Please enter the rule name you wish to get more detail of"
+Get-InboxRule -Mailbox fred.mccaig@accelerator.uk.com -Identity $MailBoxrule | Select -Property *
+
